cc82dc4601b4bb077cb99f14df7928080a082c3a
Thibaut CHARLES <cromfr@gmail.com>
2014-11-20 14:18:27 +0100

    Updated links

--- netInstallHey.sh
+++ netInstallHey.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget heman.png
-wget hey.ogg
+wget https://raw.githubusercontent.com/CromFr/heman-notify/master/heman.png
+wget https://raw.githubusercontent.com/CromFr/heman-notify/master/hey.ogg
 
-wget installHey.sh | bash
+wget https://raw.githubusercontent.com/CromFr/heman-notify/master/installHey.sh -O - | sh
 
 rm heman.png
 rm hey.ogg
