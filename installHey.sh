be5fbe117d8fdeb38dd5cd86fea8f236031b5065
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2014-11-20 14:15:00 +0100

    First !

---
+++ installHey.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#install icon
+mkdir -p $HOME/.local/share/icons
+cp heman.png $HOME/.local/share/icons
+
+#install sound
+mkdir -p $HOME/.local/share/sounds
+cp heman.png $HOME/.local/share/sounds
+
+#Add the alias to the shellrc (.bashrc for most people)
+SHRC="$HOME/.`basename $SHELL`rc"
+cat >> $SHRC << EOF
+alias n='notify-send -i heman -u critical -h string:sound-file:"$HOME/.local/share/sounds/hey.ogg" "Heyyyyyyeyeyeyeyeyeeee !"'
+EOF
+
+#Get a sample !
+notify-send -i heman -u critical -h string:sound-file:"$HOME/.local/share/sounds/hey.ogg" "Heyyyyyyeyeyeyeyeyeeee !"
+
+
+
+echo "You need to re-open a new terminal or do 'exec zsh'"
---
+++ netInstallHey.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget heman.png
+wget hey.ogg
+
+wget installHey.sh | bash
+
+rm heman.png
+rm hey.ogg
+
